0945442952052fb902222dcf6ecf4c9ffc231d27
Mai Elyamani <mai.elyamni@gmail.com>
2023-10-31 14:54:07 -0700

    add 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for c in range (122, 96, -1):
     if c % 2 == 0:
-        print(chr(c), end="")
+        print("{}".format(chr(c)), end="")
     else:
-        print(chr(c - 32), end="")
+        print("{}".format(chr(c - 32)), end="")
