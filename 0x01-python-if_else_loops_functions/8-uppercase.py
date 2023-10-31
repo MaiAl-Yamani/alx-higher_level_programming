db95a3fc320a9e5739451f18d452bc3ce3c5f141
Mai Elyamani <mai.elyamni@gmail.com>
2023-10-31 14:40:00 -0700

    add 8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for ch in range(len(str)):
+        if ord(str[ch]) in range(97, 123):
+            str_ascii = ord(str[ch]) - 32
+            new_str += chr(str_ascii)
+        else:
+            new_str += str[ch]
+            
+    print(new_str)
+    return new_str
