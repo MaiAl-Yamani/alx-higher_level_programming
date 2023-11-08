080815a3edef7e47889a79be711aea3cb2fc820a
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-08 11:12:01 -0800

    modified 101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
     return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
-#    return [[x for x in map((lambda x: x ** 2), matrix[r])] for r in range(len(matrix))]
