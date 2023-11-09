326f4a0cdcfddbebec5b22c1c6d76c0ff1e2f098
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-08 16:41:15 -0800

    add 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,53 +1,25 @@
 #!/usr/bin/python3
-def rom_val(r):
-    if r == 'I':
-        return 1
-    if r == 'V':
-        return 5
-    if r == 'X':
-        return 10
-    if r == 'L':
-        return 50
-    if r == 'C':
-        return 100
-    if r == 'D':
-        return 500
-    if r == 'M':
-        return 1000
-    return -1
-
-
 def roman_to_int(roman_string):
-    roman_list = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return 0
-    for i in range(len(roman_string)):
-        if roman_string[i] not in roman_list:
-            return 0
+    roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
     num = 0
-    i = 0
-    while (i < len(roman_string)):
-        s1 = rom_val(roman_string[i])
-        if (i + 1 < len(roman_string)):
-            s2 = rom_val(roman_string[i + 1])
-            if (s1 > s2):
-                num = num + s1
-                i = i + 1
-            else:
-                if roman_string[i] == 'I' and roman_string[i + 1] in ['V', 'X']:
-                    num = num + s2 - s1
-                    i = i + 2
-                elif roman_string[i] == 'X' and roman_string[i + 1] in ['L', 'C']:
-                    num = num + s2 - s1
-                    i = i + 2
-                elif roman_string[i] == 'C' and roman_string[i + 1] in ['D', 'M']:
-                    num = num + s2 - s1
-                    i = i + 2
-                else:
-                    num = num + s1
-                    i = i + 1
+
+    for i in range(len(roman_string)):
+        if roman_dict.get(roman_string[i], 0) == 0:
+            return 0
+        if (i != (len(roman_string) - 1) and
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+            num += -1 * roman_dict[roman_string[i]]
         else:
-            num = num + s1
-            i = i + 1
+            num += roman_dict[roman_string[i]]
     return num
