61b0f89616d46c195aef212801a0ef3230fba037
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-20 22:56:07 +0200

    modifications in 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(0, x):
+    count = 0
+    for i in range(0, x):
+        try:
             print("{}".format(my_list[i]), end='')
-            count = x
-    except Exception as e:
-        count = 0
-        for j in my_list:
+        except Exception as e:
+            break
+        else:
             count += 1
     print()
     return count
