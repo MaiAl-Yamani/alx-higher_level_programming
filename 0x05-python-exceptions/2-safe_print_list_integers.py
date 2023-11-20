b79c586955618a08c46d6c27cece6de72399f1aa
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-20 23:29:07 +0200

    add 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,6 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(0, x):
         try:
             print("{:d}".format(my_list[i]), end='')
+        except IndexError:
+            raise
         except Exception as e:
             i += 1
         else:
