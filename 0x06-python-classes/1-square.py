e1fcb2c8409abd6b2cd1783e809ba3e69e0045f9
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-22 00:16:30 +0200

    add 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a square module
+
+The module defines a simple Square class with private instance attribute: size.
+"""
+
+
+class Square:
+    """Class that defines a Square by size."""
+    def __init__(self, size=0):
+        self.__size = size
