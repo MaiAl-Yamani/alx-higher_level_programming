c173341789777845a1f74d24b695c43cb10c4b60
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-21 23:29:01 +0200

    add 0-square.py README.md

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 class Square:
     """empty class: Square"""
-    height = 0
-    weidth = 0
-
-    def __init__(self, height=0, weight=0):
-        self.Height = height
-        self.weight = weight
+    pass
