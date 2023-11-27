e71b0f457e6e4719092bd0e2cfc6f3ad8875c5fb
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-28 01:43:58 +0200

    add 6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -17,6 +17,7 @@ Method Print a message when an instance is deleted.
 class Rectangle:
     """A Recyangle class defined by its width and height."""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """
         Initialize a new rectangle.
