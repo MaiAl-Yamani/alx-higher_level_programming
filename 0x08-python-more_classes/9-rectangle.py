15a837bb02dceea4c2c3672210c466d87e151f9d
Mai Elyamani <mai.elyamni@gmail.com>
2023-11-28 02:32:43 +0200

    add 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -109,4 +109,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """Returns a rectangle with width = height = size"""
-        return cls(size, size)
+        return Rectangle(size, size)
