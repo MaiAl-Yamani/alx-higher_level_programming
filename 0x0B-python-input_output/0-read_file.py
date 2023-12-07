dbe6cf637cd703e0804178dd5d4415bd2c68b71b
Mai Elyamani <mai.elyamni@gmail.com>
2023-12-08 00:45:37 +0200

    add 0-read_file.py README.md

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is 0-read_file module."""
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding="utf-8") as myFile:
+        print(myFile.read())
