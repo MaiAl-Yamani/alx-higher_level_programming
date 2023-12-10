2571935e0d46e6aef89ef12d49d50ba882a7072b
Mai Elyamani <mai.elyamni@gmail.com>
2023-12-11 01:05:43 +0200

    add models/

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -81,3 +81,8 @@ class Rectangle(Base):
     def area(self):
         """Returns area of a rectangle."""
         return self.width * self.height
+
+    def display(self):
+        """Prints in stdout the Rectangle instance with the character #"""
+        for i in range(self.height):
+            print("#" * self.width)
