e5f5327521f47f9a790f1ada18b885d1e082fcf6
Mai Elyamani <mai.elyamni@gmail.com>
2023-12-11 00:55:28 +0200

    add models/

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,9 +8,13 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Instantiation method."""
+        self.integer_validator("width", width)
         self.width = width
+        self.integer_validator("height", height)
         self.height = height
+        self.integer_validator("x", x)
         self.x = x
+        self.integer_validator("y", y)
         self.y = y
         super().__init__(id)
 
@@ -22,6 +26,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """Width setter."""
+        self.integer_validator("width", width)
         self.__width = width
 
     @property
@@ -31,6 +36,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, height):
+        self.integer_validator("height", height)
         self.__height = height
 
     @property
@@ -41,6 +47,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, x):
         """x attribute setter."""
+        self.integer_validator("x", x)
         self.__x = x
 
     @property
@@ -51,4 +58,22 @@ class Rectangle(Base):
     @y.setter
     def y(self, y):
         """y attribute setter."""
+        self.integer_validator("y", y)
         self.__y = y
+
+    def integer_validator(self, name, value):
+        """Method that validates value.
+
+        Args:
+            name (str): name of attribute.
+            value (int): value of the attribute.
+        Raises:
+            TypeError: if value is not int.
+            ValueError: if value <= 0.
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0 and (name == "width" or name == "height"):
+            raise ValueError("{} must be > 0".format(name))
+        if value < 0 and (name == "x" or name == "y"):
+            raise ValueError("{} must be >= 0".format(name))
