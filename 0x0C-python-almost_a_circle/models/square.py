42d3f3c460b40cb7fb1441fb82b427845a5851e7
Mai Elyamani <mai.elyamni@gmail.com>
2023-12-11 20:38:07 +0200

    add models/

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,26 @@ class Square(Rectangle):
         string += str(self.x) + "/" + str(self.y) + " - "
         string += str(self.size)
         return string
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute."""
+        if not args or args is None:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+
+        for i in range(len(args)):
+            if i == 0:
+                self.id = args[i]
+            if i == 1:
+                self.size = args[i]
+            if i == 2:
+                self.x = args[i]
+            if i == 3:
+                self.y = args[i]
