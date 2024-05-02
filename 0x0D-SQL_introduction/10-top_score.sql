e4848f5b30d2771bf474b91fef30915d97ee65f6
Mai Elyamani <mai.elyamni@gmail.com>
2024-05-03 01:53:03 +0300

    deleted 0x0D. SQL - Introduction directory

--- 0x0D-SQL_introduction/10-top_score.sql
+++
@@ -1,4 +0,0 @@
--- Script that lists all records of the table second_table of the database hbtn_0c_0
--- Results displayed both the score and the name (in this order)
--- Records ordered by score (top first)
-SELECT score, name FROM second_table ORDER BY score DESC;
--- 0x0D-SQL_introduction/11-best_score.sql
+++
@@ -1,4 +0,0 @@
--- Script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
--- Results displayed both the score and the name (in this order)
--- Records ordered by score (top first)
-SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
