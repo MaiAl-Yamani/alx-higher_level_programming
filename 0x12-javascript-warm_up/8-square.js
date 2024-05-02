419191f122e9baa8b3aea92399127b4982e2cd0a
Mai Elyamani <mai.elyamni@gmail.com>
2024-05-03 01:50:03 +0300

    deleted 0x12. JavaScript - Warm up directory

--- 0x12-javascript-warm_up/8-square.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-if (isNaN(process.argv[2]) === true) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < Number(process.argv[2]); i++) {
-    console.log('X'.repeat(Number(process.argv[2])));
-  }
-}
--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-function add (a, b) {
-  if (process.argv[2] !== undefined || process.argv[3] !== undefined) {
-    console.log(Number(process.argv[2]) + Number(process.argv[3]));
-  } else {
-    console.log('NaN');
-  }
-}
-add(process.argv[2], process.argv[3]);
